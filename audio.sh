408ff29b958101ea06894588d6a48905a901535b
bitsgalore <johan.vanderknijff@kb.nl>
2016-09-02 21:27:07 +0200

    added audio CD script - needs testing

---
+++ audio.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Create simple audio CD with 1 session
+
+# Device path of cd writer (modify if needed)
+cdDevice="/dev/sr0"
+
+# Installation directory
+instDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Audio directory
+audioDir="$instDir"/data
+
+function changeCD {
+                eject $cdDevice
+                read -p "Insert a blank CD then press enter... "
+           }
+
+changeCD
+
+# Burn to disc
+wodim -v -dao -nofix dev=$cdDevice -audio -pad $audioDir/*.wav
+
